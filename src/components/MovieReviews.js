574775b355a7a4e0650f956d9877f58306c03e74
aparkening <aaron.parkening@gmail.com>
2020-01-30 17:26:08 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,19 @@
-// Code MovieReviews Here
+import React from 'react';
+import Review from './Review';
+
+const MovieReviews = ({ reviews }) => (
+  <div className="review-list">
+    {reviews.map((review, index) => <Review 
+      title={review.headline} 
+      url={review.link.url} 
+      key={review.headline+index}
+      summary={review.summary_short}
+      />) }
+  </div>
+)
+
+MovieReviews.defaultProps = {
+  reviews: []
+};
+
+export default MovieReviews;
--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
+// import SearchForm from './SearchForm'
 
 const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+            + `api-key=${NYT_API_KEY}&query=`;
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+      reviews: []
+    };
+  }
+
+  handleChange = event => this.setState({searchTerm: event.target.value});
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    fetch(URL+this.state.searchTerm)
+      .then(response => response.json())
+      .then(reviewData => this.setState({ reviews: reviewData.results }));
+  };
+
+  // Fetch giphy data and store first 3 in state
+  // fetchReviews = (query = 'star wars') => {
+  //   fetch(URL+`&query=${query}`)
+  //   .then(response => response.json())
+  //   .then(reviewData => this.setState({ reviews: reviewData.results }))
+    // .then(reviewData => console.log(reviewData))
+  // }
+
+  render() {
+    return (
+      <div className="searchable-movie-reviews">
+        <h2>Searchable Reviews</h2>
+        {/* <SearchForm fetchReviews={this.fetchReviews} /> */}
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.searchTerm} onChange={this.handleChange} />
+          <input type="submit" />
+        </form>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
+          
+export default SearchableMovieReviewsContainer;
+
