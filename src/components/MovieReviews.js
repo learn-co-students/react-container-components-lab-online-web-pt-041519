989e1e108a0cd0e4ff02b7a0bb24f624247c71eb
earthctzn <earthctzn1@gmail.com>
2020-02-01 22:30:46 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,21 @@
-// Code MovieReviews Here
+import React from 'react'
+
+
+const MovieReviews = props => {
+
+    
+   return(
+        <ol className="review-list"> 
+            { props.reviews.map( review =>
+                <li className="review" key={review.link.url + Math.random() * (10 - 1)} onClick={props.handleClick}>
+                    <h3>{review.display_title}</h3>
+                    <img src={review.multimedia.src} alt={review.display_title}></img>
+                    <p>{review.summary_short}</p> 
+                </li>
+            )}
+        </ol>
+    )
+}  
+    
+
+export default MovieReviews
